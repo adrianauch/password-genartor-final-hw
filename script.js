d98db001ec95f7331fe6218fbd675c938ebd44c3
Adrian <adrian.auch11@gmail.com>
2021-07-31 11:37:19 -0600

    completed variables and length section

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+//Global Varibales- 
+var lowerChar = "abcdefghijklmnopqrstuvwxyz";
+var upperChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var num = "0123456789";
+var specialChar = "~!@#$%^&*()_+=";
+var masterString = ""    // masterString is used to mix and match upper,lower,num and special. Use += to mix char. 
+var password = ""
+var length = 
 
 // Write password to the #password input
 function writePassword() {
@@ -12,3 +20,35 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+//function to create random password
+function generatePassword () {
+  var password = "" ; 
+  //User selects length of password
+  var passLength = prompt ("Please select the length of your password; it must be between 8 and 128 characters ");
+  var length = passLength 
+  if (passLength <8 || passLength >128){
+    //conditional - user must choose between 8 and 128 characters
+    alert("Please choose a length between 8 and 128 characters");
+    return generatePassword ()
+  }
+  //confirm uppercase characters 
+  var hasUpChar = confirm("Would you like to include UPPPERCASE characters?");
+  var hasLowcChar = confirm ("Would you like to include lowercase characters?");
+  var hasNumChar = confirm ("Would you like to include numbers?");
+  var hasSpecial = confirm ("Would you like to include special characters?");
+
+  //alert if the user does nto select characters
+  if(!hasUpChar&&!hasLowcChar&&!hasNumChar&&!hasSpecial) {
+    alert("You must choose include at least 1 character type.");
+    return generatePassword()
+  }
+  if(hasUpper){
+    password += upperChar
+  }
+  if (hasLowcChar) {
+    password += lowerChar
+  }
+
+
+}
