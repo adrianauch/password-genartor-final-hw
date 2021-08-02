5cbd47fdd42de0f1ba2d2c3763b5783ce72268c2
Adrian <adrian.auch11@gmail.com>
2021-08-01 20:12:31 -0600

    Updated README about the web application

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # password-genartor-final-hw
+
+##
+
+The web application will generate a password based on user criteria. The user is able to pick the length of password between 8 and 128 characters. If the user selects an amount outside of the specified range they will be alerted to restart. The user will then have the ability to pick if they would like their password to include upper-case letters, lower-case letters, number or special characters. The application will check to enusure that the user selected at least one type of character. The application will then produce an random password based on the users selections. The application is a adptive to display on different screen sizes.
+
+###
+
+The application runs on HTML, CSS and Javascript.
+
+###
+
+````md
+    ![photo-of-password-genarator](/Users/adrianauchterlonie/Desktop/Homework/password-genartor-final-hw/images/Screen Shot 2021-08-01 at 8.09.24 PM.png)
+    ```
+````
--- script.js
+++ script.js
@@ -7,20 +7,96 @@ var num = "0123456789";
 var specialChar = "~!@#$%^&*()_+=";
 var masterString = ""    // masterString is used to mix and match upper,lower,num and special. Use += to mix char. 
 var pickedPassword = ""
-var length = 
+var length = ""
+
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
-
+  password = pickedPassword
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
   document.querySelector("#password").textContent= pickedPassword
+  console.log(writePassword)
 
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", generatePassword (),);
+generateBtn.addEventListener("click", writePassword);
 
 //function to create random password
+function generatePassword () {
+  var pickedPassword = "" ; 
+  pickedPassword = password;
+  //User selects length of password
+  var passLength = prompt ("Please select the length of your password; it must be between 8 and 128 characters ");
+  var length = passLength 
+  if (passLength <8 || passLength >128){
+    //conditional - user must choose between 8 and 128 characters
+    alert("Please choose a length between 8 and 128 characters");
+    return generatePassword ()
+  }
+  //confirm uppercase characters 
+  var hasUpChar = confirm("Would you like to include UPPPERCASE characters?");
+  var hasLowcChar = confirm ("Would you like to include lowercase characters?");
+  var hasNumChar = confirm ("Would you like to include numbers?");
+  var hasSpecial = confirm ("Would you like to include special characters?");
+
+  //alert if the user does nto select characters
+  if(!hasUpChar&&!hasLowcChar&&!hasNumChar&&!hasSpecial) {
+    alert("You must choose include at least 1 character type.");
+    return generatePassword()
+  }
+  //gererate password based on what the user picked. 
+  if(hasUpChar && hasLowcChar && hasNumChar && specialChar){
+  masterString += upperChar + num + lowerChar + specialChar;
+  }
+  else if (hasLowcChar && hasUpChar && hasNumChar) {
+    masterString += lowerChar +upperChar + num;
+  }
+  else if(hasLowcChar && hasUpChar) {
+    masterString += upperChar + lowerChar;
+  }
+  else if (hasLowcChar) {
+    masterString += lowerChar;
+  }
+  else if (hasUpChar && hasNumChar && hasSpecial) {
+    masterString += upperChar + num + specialChar;
+  }
+  else if (hasUpChar && hasSpecial) {
+    masterString += upperChar + specialChar;
+  }
+  else if (hasUpChar && hasNumChar) {
+    masterString += upperChar + num;
+  }
+  else if (hasUpChar) {
+    masterString += upperChar;
+  }
+  else if (hasNumChar && hasSpecial) {
+    masterString += num + specialChar;
+  }
+  else if (hasNumChar) {
+    masterString += num;
+  }
+  else (hasSpecial) 
+    masterString += specialChar;
+  }
+
+
+  //for loop to generate a random password at a certain length
+  for (var i = 0; i< length; i++) {
+    var random = Math.floor(Math.random()*masterString.length);
+    pickedPassword += masterString.charAt(random);
+  }
+
+
+function writePassword () {
+  var pickedPassword = "";
+  pickedPassword = generatePassword();
+  var passwordText = document.querySelector("#password");
+  passwordText.value = pickedPassword;
+
+
+}
+  
